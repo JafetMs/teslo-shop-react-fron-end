5bee2964f4ff3675b0bc11077ea242cecac1bbfc
JafetMs <alexcero_@hotmail.com>
2026-03-19 08:39:23 -0600

    FIrst commit: Routes

---
+++ src/app.router.tsx
@@ -0,0 +1,89 @@
+import { lazy } from "react";
+import { createBrowserRouter, Navigate } from "react-router";
+
+import { ShopLayout } from "./shop/layout/ShopLayout";
+import { HomePage } from "./shop/pages/home/HomePage";
+import { ProductPage } from "./shop/product/ProductPage";
+import { GenderPage } from "./shop/pages/gender/GenderPage";
+import { LoginPage } from "./auth/pages/login/LoginPage";
+import { RegisterPage } from "./auth/pages/register/RegisterPage";
+// import { AdminLayout } from "./admin/layouts/AdminLayout";
+import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
+import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
+import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
+
+// const SearchPage = lazy(()=> import('@/heroes/pages/search/SearchPage'))
+// import { AuthLayout } from "./auth/layouts/AuthLayout";
+
+const AuthLayout = lazy(() => import("./auth/layouts/AuthLayout"));
+const AdminLayout = lazy(() =>import('./admin/layouts/AdminLayout'))
+
+
+
+export const appRouter = createBrowserRouter([
+
+    // Main routes
+    {
+        path:'/',
+        element: <ShopLayout/>,
+        children:[
+            {
+                index:true,
+                element: <HomePage/>
+            },
+            {
+                path:'product/:idSlug',
+                element: <ProductPage/>
+            },
+            {
+                path:'gender/:gender',
+                element: <GenderPage/>
+            }
+        ],
+    },
+
+
+    // Auth ROutes
+    {
+        path:'/auth',
+        element: <AuthLayout/>,
+        children:[
+            {
+                index:true,
+                element: <Navigate to="auth/login"/>
+            },
+            {
+                path:'login',
+                element: <LoginPage/>
+            },
+            {
+                path:'register',
+                element: <RegisterPage/>
+            }
+        ]
+    },
+
+    // Admin Routes
+    {
+        path: '/admin',
+        element: <AdminLayout/>,
+        children:[
+            {
+                index:true,
+                element: <DashboardPage/>,
+            },
+            {
+                path: 'products',
+                element: <AdminProductsPage/>
+            },
+            {
+                path:'product/:id',
+                element: <AdminProductPage/>
+            }
+        ]
+    },
+    {
+        path: '*',
+        element: <Navigate to='/'/>
+    }
+])
